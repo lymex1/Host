2ff1b5f6e69b3695d592e9cddf361af3cae569ef
Egor <Egor>
2023-10-30 14:36:32 +0300

    commit2

--- suppy/settings.py
+++ suppy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tfl^7azc%-^$(b^i9@e90+n@il6dlcr%t!t%ee=*$&xc3%9elv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
