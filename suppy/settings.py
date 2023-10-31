776d7e19f238b065a7139481cf68cc166025629d
Egor <Egor>
2023-10-31 21:20:16 +0300

    commit6

--- suppy/settings.py
+++ suppy/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-
+#f
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
